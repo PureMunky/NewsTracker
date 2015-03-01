a1769791c294ee4925e04034deeb1510d9bdb418
Phil Corbett <corbett.phil@gmail.com>
2015-03-01 12:21:20 -0500

    parsing html logic

---
+++ app/parse.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+exports.parse = _parse;
+exports.html = _stripHtmlTags;
+
+function _parse(data, clusterMin, clusterMax) {
+  var stripped = _stripHtmlTags(data).split(' '),
+  rtnObj = {},
+  i = 0,
+  j = 0,
+  phrase;
+
+  for (i = 0; i < stripped.length + 1 - (clusterMax - clusterMin) ; i++) {
+    phrase = '';
+
+    for (j = 0; j < clusterMax && i + j <= stripped.length; j++) {
+      phrase += (j > 0 ? ' ' : '') + stripped[i + j];
+
+      if (j + 1 >= clusterMin) {
+        if (rtnObj[phrase]) {
+          rtnObj[phrase].qty++;
+        } else {
+          rtnObj[phrase] = {
+            text: phrase,
+            qty: 1
+          }
+        }
+      }
+    }
+  }
+
+  return rtnObj;
+}
+function _stripHtmlTags(data) {
+  return data.replace(/<[^>]*>/g, ' ').replace(/\s{2,}/g, ' ').trim();
+}
---
+++ spec/parse-spec.js
@@ -0,0 +1,72 @@
+﻿var parse = require('../app/parse.js');
+
+describe('parse.js', function () {
+  it('should work - sanity test', function () {
+    expect(1).toBe(1);
+  });
+
+  it('strips out html tags - plain text', function () {
+    var html = 'testing',
+      results = parse.html(html);
+
+    expect(results).toBe(html);
+  });
+
+  it('strips out html tags - single tag', function () {
+    var text = 'test',
+      html = '<div>' + text + '</div>',
+      results = parse.html(html);
+
+    expect(results).toBe(text);
+  });
+
+  it('strips out html tags - multi nested tag', function () {
+    var text = 'test',
+      html = '<div><div>' + text + '</div></div>',
+      results = parse.html(html);
+
+    expect(results).toBe(text);
+  });
+
+  it('strips out html tags - multi non-nested tag', function () {
+    var text = 'test',
+      text2 = 'something',
+      html = '<div>' + text + '</div><div>' + text2 + '</div>',
+      results = parse.html(html);
+
+    expect(results).toBe(text + ' ' + text2);
+  });
+
+  it('groups by clusters - simple test', function () {
+    var text = 'test',
+      text2 = 'something',
+      html = '<div>' + text + '</div><div>' + text2 + '</div>',
+      results = parse.parse(html, 1, 1);
+
+    //expect(JSON.stringify(results)).toBe('');
+    expect(results[text].qty).toBe(1);
+    expect(results[text2].qty).toBe(1);
+  });
+
+  it('groups by clusters - min test', function () {
+    var text = 'test',
+      text2 = 'something',
+      html = '<div>' + text + '</div><div>' + text2 + '</div>',
+      results = parse.parse(html, 2, 2);
+
+    //expect(JSON.stringify(results)).toBe('');
+    expect(results[text + ' ' + text2].qty).toBe(1);
+  });
+
+  it('groups by clusters - min/max test', function () {
+    var text = 'test',
+      text2 = 'something',
+      html = '<div>' + text + '</div><div>' + text2 + '</div>',
+      results = parse.parse(html, 1, 2);
+
+    //expect(JSON.stringify(results)).toBe('');
+    expect(results[text].qty).toBe(1);
+    expect(results[text2].qty).toBe(1);
+    expect(results[text + ' ' + text2].qty).toBe(1);
+  });
+});
