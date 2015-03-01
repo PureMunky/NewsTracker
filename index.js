c4f2459c4e4cce2883bd5a7e71a5ccac5238de53
Phil Corbett <corbett.phil@gmail.com>
2015-03-01 18:49:54 -0500

    add configuration and scanning loop

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var config = require('./app/config.js');
 
 var app = express();
 
 app.use(express.static(__dirname));
 
 app.listen(1337);
+
+function scan() {
+  console.log('scanning');
+
+  setTimeout(scan, config.scanFrequency);
+}
+
+scan();
