fc21199034a5c8a0bfe10c6a4b02a5e4fd6448ae
Phil Corbett <corbett.phil@gmail.com>
2015-03-04 21:50:54 -0500

    correct the body fetch test

--- spec/fetch-spec.js
+++ spec/fetch-spec.js
@@ -2,10 +2,12 @@
 
 describe('fetch.js', function () {
 
+  // Sanity test.
   it('should work - sanity test', function () {
     expect(1).toBe(1);
   });
 
+  // Test local pull.
   it('should fetch locally', function (done) {
     fetch.File('index.html', function (err, data) {
       expect(data.length).toBeGreaterThan(0);
@@ -13,18 +15,21 @@ describe('fetch.js', function () {
     });
   });
 
-  //it('should fetch google', function (done) {
-  //  fetch.URL('http://www.google.com', function (res) {
-  //    expect(JSON.stringify(res)).toBe('');
-  //    done();
-  //  });
-  //});
+  // Test url pull.
+  it('should fetch www.philcorbett.net', function (done) {
+    fetch.URL('http://www.philcorbett.net', function (res) {
+      var body = '';
+      
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
 
-  //it('should fetch facebook', function (done) {
-  //  fetch.URL('http://www.facebook.com', function (res) {
-  //    expect(JSON.stringify(res)).toBe('');
-  //    done();
-  //  });
-  //});
+      res.on('end', function () {
+        expect(body.length).toBeGreaterThan(0);
+        done();
+      });
+      
+    });
+  });
 
 });
