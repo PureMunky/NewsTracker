69e00f1aceac4536dcbbd33a69eda4c2d87ec288
Phil Corbett <corbett.phil@gmail.com>
2015-03-13 06:57:53 -0400

    respect the robots.txt

--- app/fetch.js
+++ app/fetch.js
@@ -2,6 +2,7 @@
 
 var http = require('http'),
   https = require('https'),
+  robots = require('./robots.js'),
   fs = require('fs');
 
 // Processes http and https resolutions.
@@ -34,7 +35,21 @@ function _fetchURL(url, callback) {
   } else {
     callback(new Error('Not a Url'), null);
   }
-};
+}
+
+function _fetchURLConsiderRobot (url, callback) {
+  _fetchURL(robots.getLocation(url), function (err, body) {
+    if(!err) {
+      if(robots.check(body, url)){
+        _fetchURL(url, callback);
+      } else {
+        callback(new Error('Robots.txt disallow'), null);
+      }
+    } else {
+      _fetchURL(url, callback);
+    }
+  });
+}
 
 // Gets the contents of a file.
 function _fetchFile(path, callback) {
@@ -43,5 +58,5 @@ function _fetchFile(path, callback) {
   });
 }
 
-module.exports.URL = _fetchURL;
+module.exports.URL = _fetchURLConsiderRobot;
 module.exports.File = _fetchFile;
